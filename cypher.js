841190e67c8892cc0e1c31706df8827b2a9df004
matcane <mateuszcane@gmail.com>
2024-11-22 17:50:58 +0100

    fix: vigenere

--- cypher.js
+++ cypher.js
@@ -1,13 +1,20 @@
 function cipher(text, key, mode) {
+  const filteredKey =
+    typeof key === "string" ? key.replace(/[^a-zA-Z]/g, "").toUpperCase() : key;
+
+  if (typeof filteredKey === "string" && filteredKey.length === 0) {
+    return text;
+  }
+
   return text
     .toUpperCase()
     .split("")
     .map((char, i) => {
       if (/[A-Z]/.test(char)) {
         const keyOffset =
-          typeof key === "string"
-            ? key[i % key.length].toUpperCase().charCodeAt(0) - 65
-            : key;
+          typeof filteredKey === "string"
+            ? filteredKey[i % filteredKey.length].charCodeAt(0) - 65
+            : filteredKey;
         return String.fromCharCode(
           ((((char.charCodeAt(0) - 65 + keyOffset * mode) % 26) + 26) % 26) + 65
         );
