d3d3de4f70a0a39e97a1026a2a0037ac49681361
matcane <mateuszcane@gmail.com>
2024-11-23 19:54:13 +0100

    fix: vigenere & add case preservation

--- cypher.js
+++ cypher.js
@@ -5,18 +5,21 @@ function cipher(text, key, mode) {
   if (typeof filteredKey === "string" && filteredKey.length === 0) {
     return text;
   }
+  let keyIndex = 0;
 
   return text
-    .toUpperCase()
     .split("")
-    .map((char, i) => {
-      if (/[A-Z]/.test(char)) {
+    .map((char) => {
+      if (/[A-Z]/i.test(char)) {
         const keyOffset =
           typeof filteredKey === "string"
-            ? filteredKey[i % filteredKey.length].charCodeAt(0) - 65
+            ? filteredKey[keyIndex % filteredKey.length].charCodeAt(0) - 65
             : filteredKey;
+        keyIndex++;
+        const base = char === char.toUpperCase() ? 65 : 97;
         return String.fromCharCode(
-          ((((char.charCodeAt(0) - 65 + keyOffset * mode) % 26) + 26) % 26) + 65
+          ((((char.charCodeAt(0) - base + keyOffset * mode) % 26) + 26) % 26) +
+            base
         );
       } else {
         return char;
